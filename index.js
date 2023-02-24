fd83794b8aedafcb3d4e895a96403c957e91f663
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2023-02-24 00:32:06 -0300

    [Delete] Hello world

--- index.js
+++ index.js
@@ -3,8 +3,4 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo')
-})
-
 app.listen(port, console.log(`Servidor iniciado en puerto ${port}`))
