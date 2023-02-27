051601cfba2042dd840cf5f818ad7e4552a80ad2
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2023-02-27 20:48:31 -0300

    Change import index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import express from 'express';
+import app from './src/index.js';
 
-const app = express();
 const port = 3001;
-
 app.listen(port, console.log(`Servidor iniciado en puerto ${port}`))
