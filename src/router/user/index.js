37c6c3b9216568d5611f4dce212cc4de20f08e3d
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2023-03-04 17:04:05 -0300

    [add] middleware to route

--- src/router/user/index.js
+++ src/router/user/index.js
@@ -1,12 +1,13 @@
 import Express from 'express';
 import { createUser, deleteteUser } from '../../controllers/user.js';
 import { loginController } from '../../controllers/authentication.js';
+import { validateJwt } from '../../middleware/validateJwt.js';
 
 const userRouter = new Express();
 
 userRouter.post('/register', createUser)
 userRouter.post('/login', loginController)
-userRouter.delete('/:userId', deleteteUser)
+userRouter.delete('/:userId', validateJwt, deleteteUser)
 
 
 export default userRouter;
