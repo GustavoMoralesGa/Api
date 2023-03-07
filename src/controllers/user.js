4b8953a93c59eebcb93d9d9fd24c1fb7e461c63a
GustyDevs <gustavo.morales.ga@gmail.com>
2023-03-06 23:58:59 -0300

    [update] change delete logic by admin account

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,6 @@
-import { createUser as createUserModel, deleteUser } from "../models/user.js";
+import { createUser as createUserModel, 
+         deleteUser as deleteUserModel, 
+         updateUser as updateUserModel } from "../models/user.js";
 import bcrypt from 'bcryptjs';
 
 export const createUser = async (req, res) => {
@@ -17,12 +19,26 @@ export const deleteteUser = async (req, res) => {
   try {
     const { user } = req;
     if (user.role === 'ADMIN') {
-      const userId = parseInt(req.params.userId);
-      await deleteUser(userId)
+      const userId = parseInt(req.params.userId, 10);
+      await deleteUserModel(userId)
       res.status(204).send();
     } else {
       res.status(403).send({ error: 'Unauthorized' });
     }
+  } catch (e) {
+    if (e.code === 'P2025') {
+      res.status(404).send({
+        error: 'not-found',
+      })
+    } 
+    res.status(500).send(e)
+  }
+}
+
+export const updateUser = async (req, res) => {
+  try {
+    const userPayload = req.body
+    await updateUserModel(userPayload)    
   } catch (e) {
     res.status(500).send(e)
   }
