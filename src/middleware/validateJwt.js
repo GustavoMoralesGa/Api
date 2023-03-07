d769479c2c71b43213155db5cd396342d85d6a06
GustyDevs <gustavo.morales.ga@gmail.com>
2023-03-07 00:00:11 -0300

    [update] validate Jwt update

--- src/middleware/validateJwt.js
+++ src/middleware/validateJwt.js
@@ -1,19 +1,26 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
-
-const tokenRegex = /^Bearer ((?:\.?(?:[A-Za-z0-9-_]+)){3})$/gm;
 
 export const validateJwt = async (req, res, next) => {
+  const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
+  const tokenRegex = /^Bearer ((?:\.?(?:[A-Za-z0-9-_]+)){3})$/gm;
   try {
     const authHeader = req.header('Authorization');
-    const token = tokenRegex.exec(authHeader)[1];
-    jwt.verify(token, JWT_SECRET);
-    const decodedToken = jwt.decode(token);
-    req.user = decodedToken.user;
-    console.log(req.user);
+    const token = tokenRegex.exec(authHeader);
+    console.log(authHeader, token);
+    if (token) {
+      const decodedToken = jwt.decode(token[1], JWT_SECRET);
+      if (decodedToken) {
+        req.user = decodedToken.user;
+      } else {
+        throw new Error('Non valid token');
+      }
+    } else {
+      throw new Error('No token');
+    }
     next();
   } catch (e) {
+    console.log(e)
     res.status(e.code || 503).send(e)
   }
 }
