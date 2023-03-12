36716a70ed550dfa4fa8b77a3e3cfdccbf790692
GustavoMoralesGa <102779851+GustavoMoralesGa@users.noreply.github.com>
2023-03-11 23:29:58 -0300

    [create] new base route package

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,11 @@
 import Express from 'express';
-
+import packageRouter from './packages/index.js';
 import userRoutes from './user/index.js';
 
 const router = new Express();
 
 router.use('/user', userRoutes);
+router.use('/package', packageRouter)
 
 
 export default router;
