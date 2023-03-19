c770dbf06f507b08aaefe9b2d37d2df81186c83c
GustavoMoralesGa <102779851+GustavoMoralesGa@users.noreply.github.com>
2023-03-19 12:52:33 -0300

    [update] Route schedule package delete

--- src/router/schedules/index.js
+++ src/router/schedules/index.js
@@ -8,7 +8,7 @@ const scheduleRoutes = new Express();
 scheduleRoutes.post('/:packageId/schedule', validateJwt, isAdmin, createSchedule)
 scheduleRoutes.get('/schedules', validateJwt, isAdmin, getAllSchedule)
 scheduleRoutes.get('/schedules/:schedulesId')
-scheduleRoutes.delete('/:scheduleId',validateJwt, isAdmin, deleteSchedule)
+scheduleRoutes.delete('/:packageId/schedule/:scheduleId',validateJwt, isAdmin, deleteSchedule)
 scheduleRoutes.put('/:scheduleId', validateJwt, isAdmin, updateSchedule)
 
 export default scheduleRoutes;
